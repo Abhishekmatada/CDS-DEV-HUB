d2bdae4b960fc0711dfc3d4aa9737c4bb50b5427
Abhishekmatada <abhi11space96@gmail.com>
2021-03-31 02:34:24 +0530

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ZAB_DEVHUB2_VER2.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ZAB_DEVHUB2_VER2.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
